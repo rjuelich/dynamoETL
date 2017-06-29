a3dfd5d333dcb669064aaf5bcd08bae5bf5682df
EC2 Default User <ec2-user@ip-172-31-31-43.us-east-2.compute.internal>
2017-06-29 15:41:50 +0000

    Added Dynamo Table name as CLA

--- dynoImporter_dev4.py
+++ dynoImporter_dev4.py
@@ -17,13 +17,15 @@ from boto.dynamodb2.items import Item
 dynamodb = boto3.resource('dynamodb')
 
 parser = argparse.ArgumentParser(description="MongoDB JSON to dynamoDB importer")
-parser.add_argument("-j", "--json", required=True, help="JSON file name")
+parser.add_argument("-j", "--json", required=True, help="list of JSON file names to import")
+parser.add_argument("-t", "--table", required=True, help="Table name for import")
 
 args = parser.parse_args(sys.argv[1:])
 
 jsonIn = args.json
+dbTable = args.table
 
-test3 = Table('test3')
+table = Table(dbTable)
 
 with open(jsonIn, 'r') as f:
     lines = f.read().splitlines()
@@ -33,10 +35,10 @@ for line in lines:
     with open(line, 'r') as l:
         data = json.load(l)
         dynamodb_json = json.dumps(data)
-        payload = Item(test3, data=json.loads(dynamodb_json))
+        payload = Item(table, data=json.loads(dynamodb_json))
         payload.save()
         #test3.put_item(data=json.loads(dynamodb_json))
-        print line + " added to test2 dynamoDB"
+        print line + " added to" + dbTable + "dynamoDB"
         l.close()
 
 
